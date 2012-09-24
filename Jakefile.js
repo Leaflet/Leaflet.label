f2c5d60cc21f7904f716e0b36cea3c143e18f26e
jacobtoye <jacob.toye@gmail.com>
2012-09-25 11:09:52 +1200

    Adding files used to build.

---
+++ Jakefile.js
@@ -0,0 +1,65 @@
+var build = require('./build/build.js'),
+	lint = require('./build/hint.js');
+
+var COPYRIGHT = '/*\n Copyright (c) 2012, Smartrak, Jacob Toye\n' +
+	' Leaflet.label is an open-source JavaScript library for adding labels to markers and paths on leaflet powered maps.\n' +
+	' https://github.com/jacobtoye/Leaflet.label\n*/\n';
+
+desc('Check source for errors with JSHint');
+task('lint', function () {
+	var files = build.getFiles();
+
+	console.log('Checking for JS errors...');
+
+	var errorsFound = lint.jshint(files);
+
+	if (errorsFound > 0) {
+		console.log(errorsFound + ' error(s) found.\n');
+		fail();
+	} else {
+		console.log('\tCheck passed');
+	}
+});
+
+desc('Combine and compress source files');
+task('build', ['lint'], function () {
+	var pathPart = 'dist/leaflet.label',
+		srcPath = pathPart + '-src.js',
+		path = pathPart + '.js';
+
+	var files = build.getFiles();
+
+	console.log('Concatenating ' + files.length + ' files...');
+	
+	var content = build.combineFiles(files);
+
+	var oldSrc = build.load(srcPath),
+		newSrc = COPYRIGHT + content,
+		srcDelta = build.getSizeDelta(newSrc, oldSrc);
+
+	console.log('\tUncompressed size: ' + newSrc.length + ' bytes (' + srcDelta + ')');
+
+	if (newSrc === oldSrc) {
+		console.log('\tNo changes');
+	} else {
+		build.save(srcPath, newSrc);
+		console.log('\tSaved to ' + srcPath);
+	}
+
+	console.log('Compressing...');
+
+	var oldCompressed = build.load(path),
+		newCompressed = COPYRIGHT + build.uglify(content),
+		delta = build.getSizeDelta(newCompressed, oldCompressed);
+
+	console.log('\tCompressed size: ' + newCompressed.length + ' bytes (' + delta + ')');
+
+	if (newCompressed === oldCompressed) {
+		console.log('\tNo changes');
+	} else {
+		build.save(path, newCompressed);
+		console.log('\tSaved to ' + path);
+	}
+});
+
+task('default', ['build']);
---
+++ build/deps.js
@@ -0,0 +1,15 @@
+var deps = {
+	Label: {
+		src: [
+			'Label.js',
+			'Marker.Label.js',
+			'Path.Label.js',
+			'Map.Label.js'
+		],
+		desc: 'Leaflet.label plugin files.'
+	}
+};
+
+if (typeof exports !== 'undefined') {
+	exports.deps = deps;
+}
