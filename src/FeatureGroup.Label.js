1a5ddc6eb1f983f98c3d68aecdab035476d81c48
jacobtoye <jacob.toye@gmail.com>
2012-11-07 10:32:50 +1300

    Added support for label methods to FeatureGroup. fixes #2

---
+++ src/FeatureGroup.Label.js
@@ -0,0 +1,13 @@
+L.FeatureGroup.include({
+	bindLabel: function (content, options) {
+		return this.invoke('bindLabel', content, options);
+	},
+
+	unbindLabel: function () {
+		return this.invoke('unbindLabel');
+	},
+
+	updateLabelContent: function (content) {
+		this.invoke('updateLabelContent', content);
+	},
+});
