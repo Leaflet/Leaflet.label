02109d94c05b1562823de46c770a9822bbab41a1
jacobtoye <jacob.toye@gmail.com>
2012-11-07 11:01:54 +1300

    Updating build.

--- dist/leaflet.label-src.js
+++ dist/leaflet.label-src.js
@@ -245,6 +245,20 @@ L.Map.include({
 	}
 });
 
+L.FeatureGroup.include({
+	bindLabel: function (content, options) {
+		return this.invoke('bindLabel', content, options);
+	},
+
+	unbindLabel: function () {
+		return this.invoke('unbindLabel');
+	},
+
+	updateLabelContent: function (content) {
+		this.invoke('updateLabelContent', content);
+	}
+});
+
 
 
 }(this));
