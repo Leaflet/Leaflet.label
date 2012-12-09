973e38dea34a2f938995e25a8459d02d4366e650
jacobtoye <jacob.toye@gmail.com>
2012-12-10 10:21:53 +1300

    Add clearLayers method that ensures unbindLabel being called.

--- src/FeatureGroup.Label.js
+++ src/FeatureGroup.Label.js
@@ -1,4 +1,11 @@
 L.FeatureGroup.include({
+	// TODO: remove this when AOP is supported in Leaflet, need this as we cannot put code in removeLayer()
+	clearLayers: function () {
+		this.unbindLabel();
+		this.eachLayer(this.removeLayer, this);
+		return this;
+	},
+
 	bindLabel: function (content, options) {
 		return this.invoke('bindLabel', content, options);
 	},
