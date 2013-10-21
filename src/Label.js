75e95258eb2a8e4a6792f1903e477673d74455ac
jacobtoye <jacob.toye@gmail.com>
2013-10-22 11:10:08 +1300

    Round the layer point when animating the zoom. fixes #44

--- src/Label.js
+++ src/Label.js
@@ -165,7 +165,7 @@ L.Label = L.Class.extend({
 	},
 
 	_zoomAnimation: function (opt) {
-		var pos = this._map._latLngToNewLayerPoint(this._latlng, opt.zoom, opt.center);
+		var pos = this._map._latLngToNewLayerPoint(this._latlng, opt.zoom, opt.center).round();
 
 		this._setPosition(pos);
 	},
