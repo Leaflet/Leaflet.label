3b6c8ff185b4c60c3e161b20bd1e79a0ba120834
Antonino Parisi <tabman83@gmail.com>
2016-02-19 13:11:56 +0000

    Add support for HTML content.
    Add support for HTML content in a label just like the MarkerWithLabel extension for Google Maps.

--- src/Label.js
+++ src/Label.js
@@ -140,6 +140,12 @@ L.Label = (L.Layer ? L.Layer : L.Class).extend({
 			this._prevContent = this._content;
 
 			this._labelWidth = this._container.offsetWidth;
+		} else if (this._content instanceof Element) {
+			this._container.appendChild(this._content);
+
+			this._prevContent = this._content;
+
+			this._labelWidth = this._container.offsetWidth;
 		}
 	},
 
